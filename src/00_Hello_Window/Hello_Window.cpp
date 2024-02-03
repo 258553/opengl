4aa1895a5dc107fe69c38f4da894e454d6732942
yingqi li <l1931535788@163.com>
2024-02-04 00:33:14 +0800

    a day

--- src/00_Hello_Window/Hello_Window.cpp
+++ src/00_Hello_Window/Hello_Window.cpp
@@ -3,9 +3,8 @@
 
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
-
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
 
 int main()
 {
@@ -15,7 +14,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     ////glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -34,31 +33,29 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
-        //输入
+        // 输入
         processInput(window);
 
-        //渲染指令
+        // 渲染指令
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //检查并调用事件，交换缓冲
+        // 检查并调用事件，交换缓冲
         glfwPollEvents();
         glfwSwapBuffers(window);
-        
     }
     glfwTerminate();
     return 0;
 }
 
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
-    std::cout << "窗口大小改变:" << std::endl
-        << "width=" << width << "\theight=" << height << std::endl;
+    // std::cout << "窗口大小改变:" << std::endl
+    //     << "width=" << width << "\theight=" << height << std::endl;
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow* window)
+void processInput(GLFWwindow *window)
 {
     GLFW_RELEASE;
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
--- src/00_Hello_Window/Hello_Window.d
+++
@@ -1,2 +0,0 @@
-src/Hello_Window/Hello_Window.o: src/Hello_Window/Hello_Window.cpp \
- include/glad/glad.h include/KHR/khrplatform.h include/GLFW/glfw3.h
