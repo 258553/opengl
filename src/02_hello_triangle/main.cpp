b408a52db732cb95d6c24b3480c8579e7aa5066e
yingqi li <l1931535788@163.com>
2024-02-05 20:40:18 +0800

    你好三角形

--- src/02_hello_triangle/main.cpp
+++ src/02_hello_triangle/main.cpp
@@ -11,6 +11,7 @@ const char *vertexShaderSource = "#version 330 core\n"
                                  "void main()\n"
                                  "{\n"
                                  "   gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
+                                 "   gl_PointSize = 10.0f;\n"
                                  "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
@@ -28,6 +29,7 @@ int main()
     ////glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -42,23 +44,10 @@ int main()
         return -1;
     }
     glViewport(0, 0, 800, 600);
+
+    glEnable(GL_PROGRAM_POINT_SIZE);
+    // 注册窗口变化监听
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // 定义顶点数组
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f};
-
-    // 创建VBO对象(顶点缓冲对象)
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-
-    // 绑定缓冲
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    // 填充数据
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // 创建顶点和片段着色器
     unsigned int vertexShader, fragmentShader;
@@ -69,7 +58,7 @@ int main()
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
-    glShaderSource(vertexShader, 1, &fragmentShaderSource, NULL);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
 
     // 检测着色器是否编译成功
@@ -90,7 +79,7 @@ int main()
                   << infoLog << std::endl;
     }
 
-    // 创建程序对象
+    // 创建着色器程序对象
     unsigned int shaderProgram;
     shaderProgram = glCreateProgram();
 
@@ -116,6 +105,86 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    // 设置线框绘制模式
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    // // 链接顶点属性
+    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    // glEnableVertexAttribArray(0);
+
+    // // 绘制
+    // // 0.复制顶点数组到缓冲中供openGL使用
+    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // // 1.设置顶点属性指针
+    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    // glEnableVertexAttribArray(0);
+
+    // // 2.使用着色器程序
+    // glUseProgram(shaderProgram);
+    // // 3.绘制物体
+    // someOpenGLFunctionThatDrawsOurTriangle();
+
+    // 定义顶点数组
+    // float vertices[] = {
+    //     -0.5f, -0.5f, 0.0f,
+    //     0.5f, -0.5f, 0.0f,
+    //     0.0f, 0.5f, 0.0f};
+
+    // float vertices[] = {
+    //     // 第一个三角形
+    //     0.5f, 0.5f, 0.0f,  // 右上角
+    //     0.5f, -0.5f, 0.0f, // 右下角
+    //     -0.5f, 0.5f, 0.0f, // 左上角
+    //     // 第二个三角形
+    //     0.5f, -0.5f, 0.0f,  // 右下角
+    //     -0.5f, -0.5f, 0.0f, // 左下角
+    //     -0.5f, 0.5f, 0.0f   // 左上角
+    // };
+
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
+    };
+
+    unsigned int indices[] = {
+        // 注意索引从0开始!
+        // 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+        // 这样可以由下标代表顶点组合成矩形
+
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
+
+    // 创建VBO对象(顶点缓冲对象),VAO
+    unsigned int VBO, VAO, EBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // 绑定VAO对象
+    glBindVertexArray(VAO);
+
+    // 绑定VBO缓冲对象
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // 绑定EBO缓冲器
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+    // 填充数据
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // 设置顶点属性指针
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
     std::cout
         << "02_hello_triangle" << std::endl;
     while (!glfwWindowShouldClose(window))
@@ -127,10 +196,23 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        // glDrawArrays(GL_POINT, 0, 6);
+        // glDrawArrays(GL_LINE_LOOP, 0, 6);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_POINT, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_LINE_LOOP, 6, GL_UNSIGNED_INT, 0);
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glBindVertexArray(0);
+
         // 检查并调用事件，交换缓冲
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
     glfwTerminate();
     return 0;
 }
@@ -144,7 +226,6 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 
 void processInput(GLFWwindow *window)
 {
-    GLFW_RELEASE;
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
